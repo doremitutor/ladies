afb3d0b1ac5acba06e8a8fac4dafd9da412a50ee
Master <doremitutor@gmail>
2022-07-11 14:40:58 -0500

    Uploading files

---
+++ ladies.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Ladies Charm</title>
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta content="Edecanes" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+        <!-- <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" /> -->
+		<link rel="stylesheet" href="ladies.css">
+		<script src="ladies.js"></script>
+</head>
+<body>
+    <header>
+        <!-- <h1>Ladies Charm</h1> -->
+        <h1>Empezamos</h1>
+        <h2>Hay que recortar las imágenes o crearlas programáticamente para hacerlas ligeras</h2>
+        <h3> Si tus sobrinos me dejan, esta noche empiezo. Tú pégale al aspecto del negocio</h3>
+        <img src="logo2.jpeg">
+        <img src="logo1.jpeg">
+    </header>
+
+</body>
+
+</html>
